a7d4e29f65f65412495b6627ad7ac4f0e8b98087
Yui Chi Tiffany Lo <loyuichi@bu.edu>
2015-10-28 03:21:10 -0400

    Working pie chart, broken line plot; separated views

--- unitime/js/scripts.js
+++ unitime/js/scripts.js
@@ -1,21 +1,20 @@
+var dates = {};
+
 $(document).ready(function() {
-	var dates = {};
 	$.getJSON( "data.json", function( data ) {
 	  var chart = "";
 	  $.each( data["days"], function( key, vals ) {
-	  	chart += "<div class='row' data-date='" + vals["date"] + "'>";
+	  	chart += "<div class='set'><div class='row' data-date='" + vals["date"] + "'>";
 	  	for (val in vals["blocks"]) {
 	  		var cur_block = vals["blocks"][val];
 	    	chart += ( "<div class='block " + cur_block[0] + "' style=width:" + cur_block[1]*100 + "%;'>" + cur_block[1]*100 + "%</div>");
 		}
 
-		chart += "</div>";
+		chart += "</div></div>";
 
 		var date = vals["date"];
 		dates[date] = {"mind": vals["blocks"][0][1]*100, "body": vals["blocks"][1][1]*100, "world": vals["blocks"][2][1]*100, "craft": vals["blocks"][3][1]*100, "misc": vals["blocks"][4][1]*100, "weather": vals["weather"], "high": vals["high"], "low": vals["low"], "sick": vals["sick"]};
 	  });
-
-	  console.log(dates);
 
 	  $(".chart").prepend(chart);
 
@@ -26,12 +25,16 @@ $(document).ready(function() {
 			if ($(".details").length > 0) {
 				$(".details").slideUp(800).remove();
 			} else {
-				console.log("Open details");
 				var currentDate = $(this).attr("data-date");
-				console.log(currentDate);
 				var details = dates[currentDate];
 				currentRow.after("<div class='details hide'><h3>" + currentDate + "</h3>" + details["weather"] + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;H | " + details["high"] + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;L | " + details["low"] + "</div>");
+				if (currentRow.hasClass("week")) {
+					$(".details").addClass("week");
+				} else {
+					$(".details").removeClass("week");
+				}
 				$(".details").slideToggle(800);
+				$(".details").css("display", "inline-block");
 			}
 		});
 	  });
@@ -54,20 +57,59 @@ $(document).ready(function() {
 
 
 	var today = true;
+	// $(".header").click(function() {
+	// 	$(".new").slideToggle(1300, "swing", function() {
+	// 		$('.header').fadeOut(200, function() {
+	// 			if (today) {
+	//         		$(this).html('<h1 class="save">\u2014 Save</h1><h1 class="close">x Close</h1>').fadeIn(500);
+	//         		today = false;
+	//         	} else {
+	//         		$(this).html('<h1 class="today">+ Today</h1>').fadeIn(500);
+	//         		today = true;
+	//         		if ($('.resize-drag').length > 0) {
+	// 	        		addToChart();
+	// 	        	}
+	//         	}
+ //    		});
+	// 	});
+	// });
+
 	$(".today").click(function() {
 		$(".new").slideToggle(1300, "swing", function() {
-			$('.today').fadeOut(200, function() {
-				if (today) {
-	        		$(this).text('\u2014 Save').fadeIn(500);
-	        		today = false;
-	        	} else {
-	        		$(this).text('+ Today').fadeIn(500);
-	        		today = true;
-	        		if ($('.resize-drag').length > 0) {
-		        		addToChart();
-		        	}
+			$('.header').fadeOut(200, function() {
+        		$(".today").fadeOut(200, function() {
+	        		$('.save').removeClass("hide");
+	        		$('.close').removeClass("hide");
+	        	});
+        	});
+        	$('.header').fadeIn(500);
+        });
+	});
+
+	$(".save").click(function() {
+		console.log("Save");
+		$(".new").slideToggle(1300, "swing", function() {
+			$('.header').fadeOut(200, function() {
+        		$('.save').addClass("hide");
+        		$('.close').addClass("hide");
+        		$(".today").fadeIn(200);
+        		if ($('.resize-drag').length > 0) {
+	        		addToChart();
 	        	}
-    		});
+	        });
+	        $(".header").fadeIn(500);
+		});
+	});
+
+	$(".close").click(function() {
+		console.log("Close");
+		$(".new").slideToggle(1300, "swing", function() {
+			$('.header').fadeOut(200, function() {
+				$('.save').addClass("hide");
+        		$('.close').addClass("hide");
+        		$(".today").fadeIn(200);
+	        });
+	        $(".header").fadeIn(500);
 		});
 	});
 
@@ -119,36 +161,118 @@ $(document).ready(function() {
 		});
 	});
 
+	$(".overall").click(function() {
+		$(".chart").fadeOut(500, function() {
+			$(".dashboard").fadeIn(1000);
+		});
+		$(".header").fadeOut(500);
+		$(".new").fadeOut(500, function() {
+			$('.save').addClass("hide");
+    		$('.close').addClass("hide");
+        	$(".today").fadeIn(200);
+		});
+	});
+
+	$(".year").click(function() {
+		if ($(".chart").css('display') == "none") {
+			$(".dashboard").fadeOut(500, function() {
+				$(".chart").fadeIn(1000);
+				$(".header").fadeIn(1000);
+			});
+		}
+		$(".row > .block").fadeOut(600, function() {
+			$(".set").removeClass("month");
+			$(".set").removeClass("week");
+			$(".row").addClass("year");
+			$(".set").addClass("year");
+			$(".row > .block").fadeIn(2000);
+		});
+		
+	});
+
+	$(".month").click(function() {
+		if ($(".chart").css('display') == "none") {
+			$(".dashboard").fadeOut(500, function() {
+				$(".chart").fadeIn(1000);
+				$(".header").fadeIn(1000);
+			});
+		}
+
+		$(".row > .block").fadeOut(600, function() {
+			$(".set").removeClass("year");
+			$(".set").removeClass("week");
+			$(".row").addClass("month");
+			$(".details").addClass("month");
+			$(".set").addClass("month");
+			$(".row > .block").fadeIn(2000);
+		});
+		
+	});
+
+	$(".week").click(function() {
+		if ($(".chart").css('display') == "none") {
+			$(".dashboard").fadeOut(500, function() {
+				$(".chart").fadeIn(1000);
+				$(".header").fadeIn(1000);
+			});
+		}
+
+		$(".row > .block").fadeOut(600, function() {
+			$(".set").removeClass("month");
+			$(".row").addClass("week");
+			$(".details").addClass("week");
+			$(".set").addClass("week");
+			$(".row > .block").fadeIn(2000);
+		});
+		
+	});
+
+	$(".day").click(function() {
+		if ($(".chart").css('display') == "none") {
+			$(".dashboard").fadeOut(500, function() {
+				$(".chart").fadeIn(1000);
+				$(".header").fadeIn(1000);
+			});
+		}
+
+		$(".row > .block").fadeOut(600, function() {
+			$(".set").removeClass("month");
+			$(".row").removeClass("week");
+			$(".details").removeClass("week");
+			$(".set").removeClass("week");
+			$(".row > .block").fadeIn(2000);
+		});
+	});
+
 	var totals = [
-	  { category: 'mind',  count: 0 },
-	  { category: 'body',  count: 0 },
-	  { category: 'world',  count: 0 },
-	  { category: 'craft',  count: 0 },
-	  { category: 'misc', count: 0 }
+	  { category: 'mind',  count: 201 },
+	  { category: 'body',  count: 138 },
+	  { category: 'world',  count: 109 },
+	  { category: 'craft',  count: 270 },
+	  { category: 'misc', count: 182 }
 	];
 
-	for (date in dates) {
-		totals[0].count = totals[0].count + date[totals[0].category];
-		totals[1].count += date[totals[1].category];
-		totals[2].count += date[totals[2].category];
-		totals[3].count += date[totals[3].category];
-		totals[4].count += date[totals[4].category];
-	}
+	// console.log(dates);
+	// for (var date in dates) {
+	// 	console.log(date);
+	// 	totals[0].count = totals[0].count + date[totals[0].category];
+	// 	totals[1].count += date[totals[1].category];
+	// 	totals[2].count += date[totals[2].category];
+	// 	totals[3].count += date[totals[3].category];
+	// 	totals[4].count += date[totals[4].category];
+	// }
 
-	console.log(totals);
+	//console.log(totals);
 
 	var pie = d3.layout.pie().value(function(d) { return d.count })
 
 	var slices = pie(totals);
 
-	var arc = d3.svg.arc().innerRadius(0).outerRadius(50);
-
-	// helper that returns a color based on an ID
-	var color = d3.scale.category10();
+	var arc = d3.svg.arc().innerRadius(0).outerRadius(150);
 
 	var svg = d3.select('svg.pie');
 	var g = svg.append('g')
-	  .attr('transform', 'translate(200, 50)')
+	  .attr('transform', 'translate(150, 150)')
 
 	g.selectAll('path.slice')
 	  .data(slices)
@@ -156,7 +280,39 @@ $(document).ready(function() {
 	      .append('path')
 	        .attr('class', 'slice')
 	        .attr('class', function(d) {
-	        	return d.data.product;
+	        	return d.data.category;
 	        })
 	        .attr('d', arc);
+	        // .attr('fill', function(d) {
+	        //   return color(d.data.product);
+	        // });
+
+	var data = [
+      { date: '2014-01-01', amount: 10 },
+      { date: '2014-02-01', amount: 20 },
+      { date: '2014-03-01', amount: 40 },
+      { date: '2014-04-01', amount: 80 }
+    ];
+
+	var x = d3.time.scale()
+	    .domain([
+	      new Date(Date.parse('2014-01-01')),
+	      new Date(Date.parse('2014-04-01'))
+	    ])
+	    .range([0, 300]);
+
+	//x(new Date(Date.parse('2014-02-01')));
+	// 103.3811949976841
+	var xAxis = d3.svg.axis()
+	  .scale(x)         // x is the d3.time.scale()
+	  .orient('bottom') // the ticks go below the graph
+	  .ticks(6);        // specify the number of ticks
+
+	var linegraph = d3.select('svg.line')   // create an <svg> element
+	    .attr('width', 500) // set its dimentions
+	    .attr('height', 150);
+
+	linegraph.append('g')            // create a <g> element
+	  .attr('class', 'x axis') // specify classes
+	  .call(xAxis); 
 });
