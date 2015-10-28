0e69d69d462e43459a227f099629d3bf1efc1da2
Yui Chi Tiffany Lo <loyuichi@bu.edu>
2015-10-28 01:11:03 -0400

    d3 build

--- unitime/js/scripts.js
+++ unitime/js/scripts.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
-	var dates = [];
+	var dates = {};
 	$.getJSON( "data.json", function( data ) {
-	  console.log("Here");
 	  var chart = "";
 	  $.each( data["days"], function( key, vals ) {
 	  	chart += "<div class='row' data-date='" + vals["date"] + "'>";
@@ -13,7 +12,7 @@ $(document).ready(function() {
 		chart += "</div>";
 
 		var date = vals["date"];
-		dates[date] = {"weather": vals["weather"], "high": vals["high"], "low": vals["low"], "sick": vals["sick"]};
+		dates[date] = {"mind": vals["blocks"][0][1]*100, "body": vals["blocks"][1][1]*100, "world": vals["blocks"][2][1]*100, "craft": vals["blocks"][3][1]*100, "misc": vals["blocks"][4][1]*100, "weather": vals["weather"], "high": vals["high"], "low": vals["low"], "sick": vals["sick"]};
 	  });
 
 	  console.log(dates);
@@ -25,14 +24,14 @@ $(document).ready(function() {
 			$(this).toggleClass("row-expanded");
 			var currentRow = $(this);
 			if ($(".details").length > 0) {
-				$(".details").slideUp("slow").remove();
+				$(".details").slideUp(800).remove();
 			} else {
 				console.log("Open details");
 				var currentDate = $(this).attr("data-date");
 				console.log(currentDate);
 				var details = dates[currentDate];
 				currentRow.after("<div class='details hide'><h3>" + currentDate + "</h3>" + details["weather"] + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;H | " + details["high"] + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;L | " + details["low"] + "</div>");
-				$(".details").slideToggle("slow");
+				$(".details").slideToggle(800);
 			}
 		});
 	  });
@@ -101,4 +100,63 @@ $(document).ready(function() {
 			console.log($(this).width());
 		});
 	});
+
+	$(".toolbar-scaleopts.medium").click(function() {
+		console.log("Medium");
+		$(".row > .block").fadeOut(600, function() {
+			$(".row").addClass("week");
+			$(".row > .block").fadeIn(2000);
+		});
+		
+	});
+
+	$(".toolbar-scaleopts.large").click(function() {
+		console.log("Medium");
+		$(".row > .block").fadeOut(600, function() {
+			$(".row").removeClass("week");
+			$(".row").addClass
+			$(".row > .block").fadeIn(2000);
+		});
+	});
+
+	var totals = [
+	  { category: 'mind',  count: 0 },
+	  { category: 'body',  count: 0 },
+	  { category: 'world',  count: 0 },
+	  { category: 'craft',  count: 0 },
+	  { category: 'misc', count: 0 }
+	];
+
+	for (date in dates) {
+		totals[0].count = totals[0].count + date[totals[0].category];
+		totals[1].count += date[totals[1].category];
+		totals[2].count += date[totals[2].category];
+		totals[3].count += date[totals[3].category];
+		totals[4].count += date[totals[4].category];
+	}
+
+	console.log(totals);
+
+	var pie = d3.layout.pie().value(function(d) { return d.count })
+
+	var slices = pie(totals);
+
+	var arc = d3.svg.arc().innerRadius(0).outerRadius(50);
+
+	// helper that returns a color based on an ID
+	var color = d3.scale.category10();
+
+	var svg = d3.select('svg.pie');
+	var g = svg.append('g')
+	  .attr('transform', 'translate(200, 50)')
+
+	g.selectAll('path.slice')
+	  .data(slices)
+	    .enter()
+	      .append('path')
+	        .attr('class', 'slice')
+	        .attr('class', function(d) {
+	        	return d.data.product;
+	        })
+	        .attr('d', arc);
 });
