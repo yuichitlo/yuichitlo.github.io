0a6050d3ee6b7efe0727103b27f1d60824bf46b2
Tiffany <yuichitlo@users.noreply.github.com>
2024-06-05 01:44:57 -0400

    Switch to 2-column layout; Update project photos

--- js/nav.js
+++ js/nav.js
@@ -1,22 +1,15 @@
 var toggleNavOverlay = function(show) {
     if (show) {
         document.querySelector(".nav-overlay").classList.add('show');
-        document.querySelector(".nav-overlay").classList.remove('hide');
     }
     else {
         document.querySelector(".nav-overlay").classList.remove('show');
-        document.querySelector(".nav-overlay").classList.add('hide');
     }
 };
 
 window.addEventListener('load', function () {
     document.querySelector('.nav-toggle').addEventListener('click', function(event) {
         toggleNavOverlay(true);
-        event.preventDefault();
-    });
-
-    document.querySelector('.nav-overlay-close').addEventListener('click', function(event) {
-        toggleNavOverlay(false);
         event.preventDefault();
     });
 }, false);
