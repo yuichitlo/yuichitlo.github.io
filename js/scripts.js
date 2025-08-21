a056aaaf8452407f1f7d00718c160319eec4ed3e
Tiffany <yuichitlo@users.noreply.github.com>
2025-08-21 16:47:50 -0400

    Gen Bakes updates

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// document.addEventListener("DOMContentLoaded", (event) => {
+//     gsap.registerPlugin(ScrollTrigger);
+//     gsap.to('.scroll-div', {
+//         // scrollTrigger: '.scroll-div',
+//         // scrollTrigger: {
+//         //     trigger: '.homepage-scroll',
+//         //     start: 'top center',
+//         //     scrub: 1,
+//         //     markers: true,
+//         // },
+//         yPercent: -75,
+//         // repeat: -1,
+//         // repeatDelay: 0.5, // seconds between repeats
+//         delay: 5,
+//         ease: 'power3.inOut',
+//         duration: 5,
+//     });
+//     ScrollTrigger.refresh();
+// });
